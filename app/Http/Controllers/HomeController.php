87cb87f2b05162eb959a40d76fdee701d54c961a
nakashi94 <n0904yBUNJOU@gmail.com>
2022-02-24 09:51:42 +0900

    create tag function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,6 +44,14 @@ class HomeController extends Controller
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
 
+        //同一のタグがあるか確認
+        $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
+        if (empty($exist_tag['id'])) {
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+        } else {
+            $tag_id = $exist_tag['id'];
+        }
+
         $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
 
         $memo_id = Memo::insertGetId([
