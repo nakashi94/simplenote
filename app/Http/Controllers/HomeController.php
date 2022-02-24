d71063fe00563cec1c3b0ab8fa213982e832ad54
nakashi94 <n0904yBUNJOU@gmail.com>
2022-02-24 10:16:52 +0900

    create delete method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
     {
         $user = \Auth::user();
         $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        return view('home', compact('user', 'memos'));
+        return view('create', compact('user', 'memos'));
     }
 
     public function create()
@@ -81,5 +81,12 @@ class HomeController extends Controller
         $inputs = $request->all();
         Memo::where('id', $id)->update(['content' => $inputs['content'], 'tag_id' => $inputs['tag_id']]);
         return redirect()->route('home');
+    }
+
+    public function delete(Request $request, $id)
+    {
+        $inputs = $request->all();
+        Memo::where('id', $id)->update(['status' => 2]);
+        return redirect()->route('home')->with('success', 'メモの削除が完了しました。');
     }
 }
